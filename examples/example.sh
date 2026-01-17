bfe232052b51ca5437bbf39e56492b194ba1cdb7
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2026-01-17 13:41:14 +0100

    Update with new features

--- examples/example.sh
+++ examples/example.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-#SBATCH --job-name=foobar          # job name (required; used below: '%x')
+# project structure: 
+#
+# ~/projects/foobar/
+# |├── foobar.sh
+# |├── foobar.R
+# |├── runs/
+# |   |└── 20498908/
+# |       |├── stdout.log
+# |       |├── stderr.log
+# |       |├── env.txt
+# |       |├── sessioninfo.txt
+# |       |└── results/
+#
+# Important: create /run before your submit!
+
+#SBATCH --job-name=foobar          # job name 
                                
 #SBATCH --time=00:05:00            # format:HH:MM:SS
                                    # Tip: add ~10% safety margin, because a high
                                    # load might negatively impact runtime of your job(s)!
 
-#SBATCH --mem=1GB                   # options: K M G T 
+#SBATCH --mem=1G                   # options: K M G T 
 
-# #SBATCH --partition=epyc-768    # see: sumpartpro output for more
+#SBATCH --partition=epyc-768       # see: lsparts output for more
                                    # use for projects or specfic parititons
 
-# ressource layout: global
+# important: you can only use one ressource layout!
+
+# ressource layout: global         # slurm decides the placement across nodes
 # #SBATCH --ntasks=1               # total number of tasks (processes)
-# (commented out)                   # e.g. 24 independent R processes
-                                    # let slurm decide the placement across nodes
-                                   
+# (commented out)                  # e.g. 24 independent R processes
+
 # ressource layout: local
 #SBATCH --nodes=1                   # allocate exactly 1 physical node
 #SBATCH --ntasks-per-node=1         # place 1 task(s)/processe(s) on that node
@@ -25,13 +41,14 @@
 
 #SBATCH --hint=nomultithread        # avoid hardware hyper-threading (intel) / smt (amd)
 
-#SBATCH --output=$HOME/outgoing/%x-%j.out    # %x: job name %j: jobid
-#SBATCH --error=$HOME/outgoing/%x-%j.err     # %x: job name %j: jobid  
+#SBATCH --output=runs/%j/stdout.log   # create ./runs/%j/stdout.log
+#SBATCH --error=runs/%j/stderr.log    # create ./runs/%j/stderr.log
+                                      # %j: jobid (%x: name - seems not to work)
 
-#SBATCH --mail-type=END                # options: END, FAIL, ALL
+#SBATCH --mail-type=END              # options: END, FAIL, ALL
 
-# Option "NONE" does not work
-# #SBATCH --export=NONE              # start job in a clean environment 
+                                     # Option "NONE" does not work (?)
+#SBATCH --export=ALL                 # start job in a clean environment 
                                      # no inherited shell vars (only SLURM_*)
                                      # variables defined below (see: export)
                                      # default: ALL (all variables inherited)
@@ -47,22 +64,31 @@ set -euo pipefail                      # strict shell mode: fail fast on
 
 # --- files / logging ---
 
-# mkdir -p "$HOME/outgoing"
+RUN_DIR="$SLURM_SUBMIT_DIR/runs/$SLURM_JOB_ID" 
+mkdir -p "$RUN_DIR"                             # create per-job run directory 
+cd "$RUN_DIR"                                   # switch into it
+
+# create folder inside RUN_DIR/<jobid>
+mkdir -p results                                # store the results 
+mkdir -p scripts                                # keep copy of original scripts 
+
+cp -f "$SLURM_SUBMIT_DIR/foobar.sh" scripts/ || true
+cp -f "$SLURM_SUBMIT_DIR/foobar.R"  scripts/ || true
 
 # --- logging (start) ---
 
-echo "Job ID: $SLURM_JOB_ID"                          # unique job identifier
-echo "Name: $SLURM_JOB_NAME"                          # job name (%x)
-echo "Node: ${SLURMD_NODENAME:-$SLURM_NODELIST}"      # single node or fallback: compact nodelist
-echo "Partition: $SLURM_JOB_PARTITION"                # partition used
-echo "start: $(date -Is)"                             # ISO-8601 start time
+echo "Job ID: $SLURM_JOB_ID"                        # unique job identifier
+echo "Name: $SLURM_JOB_NAME"                        # job name (%x)
+echo "Node: ${SLURMD_NODENAME:-$SLURM_NODELIST}"    # single node or fallback: compact nodelist
+echo "Partition: $SLURM_JOB_PARTITION"              # partition used
+echo "start: $(date -Is)"                           # ISO-8601 start time
 echo
 
 # --- modules ---
 
 module purge
 module load R/latest       # version, e.g., in r session info
-# module load R/4.3         # set version explicitly
+# module load R/4.3        # set version explicitly
 
 # --- environment ---
 
@@ -75,17 +101,17 @@ export OPENBLAS_NUM_THREADS="$THREADS"
 export NUMEXPR_NUM_THREADS="$THREADS"
 export STAN_NUM_THREADS="$THREADS"
 
-# Optional: record environment for reproducibility
-env | sort > "$HOME/outgoing/env-${SLURM_JOB_NAME}-${SLURM_JOB_ID}.txt"
+# optional: record environment for reproducibility
+env | sort > "env.txt" # record environment: reproducibility
 
 # --- run ---
-
-# Put the following on top of the R script:
-# outdir <- Sys.getenv("OUTDIR", ".")
-# sink(file.path(outdir,
-#     sprintf("rsesinfo-%s.txt", Sys.getenv("SLURM_JOB_ID"))))
-# sessioninfo::session_info()
-# sink()
+#
+# put the following at the top of simreplicabim.R:
+# writeLines(
+#   capture.output(utils::sessionInfo()),
+#   "sessioninfo.txt"
+#)
+# output goes to stdout.log
 
 # options(mc.cores = 4)
 # fit <- mod$sample(
@@ -96,7 +122,7 @@ env | sort > "$HOME/outgoing/env-${SLURM_JOB_NAME}-${SLURM_JOB_ID}.txt"
 # 4 chains x 1 core/chain = 4 cores total
 #
 
-Rscript example.R 
+srun Rscript "$SLURM_SUBMIT_DIR/foobar.R"
 
 # --- logging (end) ---
 
